7510b2e2fa3ce6381d8ce51ccd92b500a6dd70fd
Grant Achuzia <grant.achuzia@gmail.com>
2026-02-28 02:22:11 -0500

    remove old sh file

--- build_sim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Cadmium path on devssim
-CADMIUM_PATH="/home/cadmium/rt_cadmium/include"
-
-# Compile
-g++ -g -O3 -std=c++17 ./main/main.cpp -I $CADMIUM_PATH -o simulation
-
-echo "Build successful! Run it using: ./simulation"
